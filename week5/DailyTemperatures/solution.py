636ec2db6d15536925be74b7eeb3c7e721adb9e9
Noel Paing Oak Soe <159676867+NoelPOS@users.noreply.github.com>
2025-12-09 01:00:44 +0700

    week 5: daily temperatures

---
+++ week5/DailyTemperatures/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        ans = [0] * n
+        stack = []
+        
+        for i in range(n):            
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev = stack.pop()
+                days = i - prev
+                ans[prev] = days
+            
+            stack.append(i)
+        
+        return ans
